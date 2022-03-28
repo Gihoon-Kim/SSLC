9d28dc6644def60a5c1e977741b8f004d464cdff
Gihoon Kim <gkim61@myseneca.ca>
2022-03-27 20:30:04 -0400

    Delete

--- app/src/main/java/com/example/sslc/adapters/ClassFragmentAdapter.java
+++ app/src/main/java/com/example/sslc/adapters/ClassFragmentAdapter.java
@@ -1,21 +1,31 @@
 package com.example.sslc.adapters;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 import com.example.sslc.AdminClassDetailActivity;
 import com.example.sslc.R;
 import com.example.sslc.data.Programs;
+import com.example.sslc.requests.DeleteClassRequest;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -24,6 +34,7 @@ import butterknife.ButterKnife;
 
 public class ClassFragmentAdapter extends RecyclerView.Adapter<ClassFragmentAdapter.ClassFragmentViewHolder> {
 
+    private static final String TAG = "ClassFragmentAdapter";
     ArrayList<Programs> programDataList;
     Context context;
     ActivityResultLauncher<Intent> updateClassResultLauncher;
@@ -77,6 +88,56 @@ public class ClassFragmentAdapter extends RecyclerView.Adapter<ClassFragmentAdap
 
             updateClassResultLauncher.launch(intent);
         });
+
+        holder.cv_Item.setOnLongClickListener(view -> {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setMessage("Do you really want to remove this News?")
+                    .setTitle("Delete News : " + programDataList.get(holder.getAdapterPosition()).getProgramTitle())
+                    .setPositiveButton("Delete", (dialogInterface, i) ->
+                            deleteClassFromListAndDatabase(holder.getAdapterPosition(), programDataList.get(holder.getAdapterPosition()).getProgramNumber()))
+                    .setNegativeButton("Cancel", null)
+                    .show();
+            return false;
+        });
+    }
+
+    private void deleteClassFromListAndDatabase(int position, int classNumber) {
+
+        ProgressDialog progressDialog = new ProgressDialog(context);
+        progressDialog.setTitle("Delete Class");
+        progressDialog.setMessage("Please Wait.\nDeleting in progress");
+        progressDialog.show();
+
+        @SuppressLint("NotifyDataSetChanged") Response.Listener<String> responseListener = response -> {
+
+            try {
+
+                Log.i(TAG, response);
+                JSONObject jsonResponse = new JSONObject(response);
+                boolean success = jsonResponse.getBoolean("success");
+
+                if (success) {
+
+                    programDataList.remove(position);
+                    notifyDataSetChanged();
+                } else {
+
+                    Toast.makeText(context, "Delete Class Failed", Toast.LENGTH_SHORT).show();
+                }
+                progressDialog.dismiss();
+            } catch (Exception e) {
+
+                e.printStackTrace();
+            }
+        };
+
+        DeleteClassRequest deleteClassRequest = new DeleteClassRequest(
+                classNumber,
+                responseListener
+        );
+        RequestQueue queue = Volley.newRequestQueue(context);
+        queue.add(deleteClassRequest);
     }
 
     @Override
